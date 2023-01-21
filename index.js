94e6c3d2975794480ed3beb66c20e0fce61dbc65
AngelikaGlaser <angelikaglaser2001@gmail.com>
2023-01-21 22:07:30 +0200

    Added an error for when no city name is inserted. - correct

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ const fetch = require('node-fetch')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-//proceed from post method data
-app.use(express.json())
-app.use(express.urlencoded({extended : true}))
-
 const key = '9f188c8987e25d0ea648592653f715c3';
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 const getWeatherDataPromise = (url) => {
     return new Promise((resolve, reject) => {
@@ -38,8 +37,11 @@ const getWeatherDataPromise = (url) => {
 
 app.all('/', function (req, res) {
     let city
-    if (req.method === 'POST' && req.body.cityname) {
-        city = req.body.cityname;
+    if(req.method == 'GET'){
+        city = 'Tartu'
+    }
+    if(req.method == 'POST'){
+        city = req.body.cityname
         if(city == ""){
             res.render('index', {error: "Please enter city name"})
         }
